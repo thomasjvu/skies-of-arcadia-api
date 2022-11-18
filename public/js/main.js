6b128decd1fa58de2cc23949d52e96a163514478
Thomas Vu <thomasjvu@gmail.com>
2022-11-18 03:04:49 -0500

    Update endpoint

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ document.querySelector('button').addEventListener('click', getSOA)
 async function getSOA() {
     const query = document.querySelector('input').value 
     try {
-        const res = await fetch(`https://skies-of-arcadia-api.onrender.com/api/characters/${query}`)
+        const res = await fetch(`https://https://skies-of-arcadia.cyclic.app/api/characters/${query}`)
         const data = await res.json()
         console.log(data)
         document.querySelector('h3').innerText = data.name
