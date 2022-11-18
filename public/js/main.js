beb13e0b37ebab2b29af93243b4450b325930b41
Thomas Vu <thomasjvu@gmail.com>
2022-11-18 03:10:40 -0500

    Rename functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,11 @@
 console.log('"Impossible is just a word quitters use to feel good about themselves!" - Vyse')
 
-document.querySelector('button').addEventListener('click', getSOA)
+document.querySelector('button').addEventListener('click', getCharacter)
 
-async function getSOA() {
-    const query = document.querySelector('input').value 
+async function getCharacter() {
+    const character = document.querySelector('input').value 
     try {
-        const res = await fetch(`https://https://skies-of-arcadia.cyclic.app/api/characters/${query}`)
+        const res = await fetch(`https://https://skies-of-arcadia.cyclic.app/api/characters/${character}`)
         const data = await res.json()
         console.log(data)
         document.querySelector('h3').innerText = data.name
