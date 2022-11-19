8a40b43ac614322815339afcbc5359ed253e9014
Thomas Vu <thomasjvu@gmail.com>
2022-11-18 22:17:06 -0500

    Update fetch endpoint

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ document.querySelector('button').addEventListener('click', getCharacter)
 async function getCharacter() {
     const character = document.querySelector('input').value 
     try {
-        const res = await fetch(`skies-of-arcadia.cyclic.app/api/characters/${character}`)
+        const res = await fetch(`/api/characters/${character}`)
         const data = await res.json()
         console.log(data)
         document.querySelector('h3').innerText = data.characters.name
